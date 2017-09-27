f2c68fa97c8ae142a0a07f89423635b2427633c5
jalbertomr <jalbertomr@yahoo.com>
2017-09-27 18:41:27 -0500

    se agrega projecto DemoApp1 ejemplo de Windows System API

---
+++ DemoApp1/DemoModule/build/classes-generated/org/netbeans/demo/demomodule/Bundle.java
@@ -0,0 +1,27 @@
+package org.netbeans.demo.demomodule;
+/** Localizable strings for {@link org.netbeans.demo.demomodule}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Demo</i>
+     * @see DemoTopComponent
+     */
+    static String CTL_DemoAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_DemoAction");
+    }
+    /**
+     * @return <i>Demo Window</i>
+     * @see DemoTopComponent
+     */
+    static String CTL_DemoTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_DemoTopComponent");
+    }
+    /**
+     * @return <i>This is a Demo window</i>
+     * @see DemoTopComponent
+     */
+    static String HINT_DemoTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_DemoTopComponent");
+    }
+    private Bundle() {}
+}
